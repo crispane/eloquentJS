4ee60c67c245785278742a9e32eab589d9378986
crispane <chpanagakos@gmail.com>
2022-12-18 19:34:25 +0200

    housekeeping

---
+++ TESTBENCH/TESTING/testing.js
@@ -0,0 +1,19 @@
+console.log("//Script to testing.html");
+console.log("");
+
+function test(label, body) {
+	if (!body()) console.log(`Failed: ${label}`);
+	else console.log(`Success: ${label}`);
+}
+
+test("convert Latin text to uppercase", () => {
+	return "hello".toUpperCase() == "HELLO";
+});
+
+test("convert Greek text to uppercase", () => {
+	return "Χαίρετε".toUpperCase() == "ΧΑΊΡΕΤΕ";
+});
+
+test("don't convert case-less characters", () => {
+	return "ابحرم".toUpperCase() == "ابحرم";
+});
