342660cf376e7f5ef65ac5c68b2f790ac10e9813
crispane <chpanagakos@gmail.com>
2023-02-02 12:21:58 +0200

    Remove styling from JS

--- Chapter 14/BUILD-A-TABLE/build-a-table.js
+++ Chapter 14/BUILD-A-TABLE/build-a-table.js
@@ -10,9 +10,8 @@ const MOUNTAINS = [
 
 function buildTable(data) {
 	let table = document.createElement("table");
-
-	let fields = Object.keys(data[0]);
 	let headRow = document.createElement("tr");
+	let fields = Object.keys(data[0]);
 
 	fields.forEach(function (field) {
 		let headCell = document.createElement("th");
@@ -26,14 +25,10 @@ function buildTable(data) {
 		fields.forEach(function (field) {
 			let cell = document.createElement("td");
 			cell.appendChild(document.createTextNode(object[field]));
-			if (typeof object[field] == "number") {
-				cell.style.textAlign == "right";
-			}
 			row.appendChild(cell);
 		});
 		table.appendChild(row);
 	});
-
 	return table;
 }
 
