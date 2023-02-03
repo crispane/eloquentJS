25bd7a26121cf706d4b174723953740b7a3fff88
crispane <chpanagakos@gmail.com>
2023-02-03 08:20:04 +0200

    Add JS

--- Chapter 14/ELEMENTS-BY-TAG-NAME/elements-by-tag-name.js
+++ Chapter 14/ELEMENTS-BY-TAG-NAME/elements-by-tag-name.js
@@ -1,3 +1,20 @@
 function byTagName(node, tagName) {
+	let found = [];
+	tagName = tagName.toUpperCase();
 
+	function explore(node) {
+		for (let i = 0; i < node.childNodes.length; i++) {
+			let child = node.childNodes[i];
+			if (child.nodeType == document.ELEMENT_NODE) {
+				if (child.nodeName == tagName) found.push(child);
+				explore(child);
+			}
+		}
+	}
+	explore(node);
+	return found;
 }
+
+console.log(byTagName(document.body, "h1").length);
+console.log(byTagName(document.body, "span").length);
+
