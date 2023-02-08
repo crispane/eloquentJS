1f5244e2a69b76cb2697b2d34d0201b3169db77d
crispane <chpanagakos@gmail.com>
2023-02-08 16:34:10 +0200

    Add resizable bar

--- TESTBENCH/PRIMITIVE_DRAWING/drawing.js
+++ TESTBENCH/PRIMITIVE_DRAWING/drawing.js
@@ -5,3 +5,26 @@ window.addEventListener("click", event => {
 	dot.style.top = (event.pageY - 4) + "px";
 	document.body.appendChild(dot);
 })
+
+let lastX; // Tracks the last observed mouse X position
+let bar = document.querySelector("div");
+bar.addEventListener("mousedown", event => {
+	if (event.button == 0) {
+		lastX = event.clientX;
+		window.addEventListener("mousemove", moved);
+		event.preventDefault(); // Prevent selection
+	}
+});
+
+function moved(event) {
+	if (event.buttons == 0) {
+		window.removeEventListener("mousemove", moved);
+	} else {
+		let dist = event.clientX - lastX;
+		let newWidth = Math.max(10, bar.offsetWidth + dist);
+		let newHeight = Math.max(10, bar.offsetHeight + dist);
+		bar.style.width = newWidth + "px";
+		bar.style.height = newHeight + "px";
+		lastX = event.clientX;
+	}
+}
